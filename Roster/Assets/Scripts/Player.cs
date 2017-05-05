6cf69c6b4cdcada03e10d6c0629c2ae6b0a2cf20
Mivizal <miran.sarban@gmail.com>
2017-05-05 12:58:03 +0200

    First Commit
    Roster fight first commit.

---
+++ Roster/Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))] //Sets a condition where it requires Controller2D script to run. 
+
+public class Player : MonoBehaviour
+{
+
+    Controller2D controller;
+
+    // Use this for initialization
+    void Start()
+    {
+        controller = GetComponent<Controller2D> ();
+    }
+}	
